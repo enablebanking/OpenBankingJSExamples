5c0ca6dc4f1a5a6767125c42edc8f10efdad0ce3
Pavel <pavel@enablebanking.com>
2021-01-18 16:49:45 +0200

    update aisp example

--- aispdemo/index.js
+++ aispdemo/index.js
@@ -3,204 +3,163 @@
 const enablebanking = require('enablebanking');
 const readline = require('readline');
 
+
+const REDIRECT_URL = "https://enablebanking.com/auth_redirect"  // PUT YOUR REDIRECT URI HERE
+const CONNECTOR_NAME = "Nordea"
+const CONNECTOR_COUNTRY = "FI"
 /**
  * Bank connector specific settings
  */
-const settings = {
-  'Nordea': enablebanking.NordeaConnectorSettings.constructFromObject({
-    sandbox: true,
-    consentId: null,
-    accessToken: null,
-    redirectUri: 'https://enablebanking.com/auth_redirect',
-    country: null, // to be set when country is chosen
-    clientId: '!!! CLIENT ID TO BE INSERTED HERE !!!',
-    clientSecret: '!!! CLIENT SECRET TO BE INSERTED HERE !!!',
-    signKeyPath: 'path/to/signature/key.pem',
-    sessionDuration: 1000,
-    language: null
-  }),
-  'Swedbank': enablebanking.SwedbankConnectorSettings.constructFromObject({
-    sandbox: true,
-    consentId: null,
-    accessToken: null,
-    redirectUri: 'https://enablebanking.com/auth_redirect',
-    country: null, // to be set when country is chosen
-    clientId: '!!! CLIENT ID TO BE INSERTED HERE !!!',
-    clientSecret: '!!! CLIENT SECRET TO BE INSERTED HERE !!!'
-  })
-  // More settings to be put here
-};
-
-/**
- * Helper function for reading a single line from stdin
- */
-async function readLine(msg) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  console.log(msg);
-  for await (const line of rl) {
-    return line;
-  }
+const CONNECTOR_SETTINGS = {
+  sandbox: true,
+  consentId: null,
+  accessToken: null,
+  redirectUri: REDIRECT_URL,
+  country: CONNECTOR_COUNTRY,
+  business: null,
+  clientId: "client_id",  // API Client ID
+  clientSecret: "client_secret",  // API Client Secret
+  signKeyPath: "/path/to/private.key",  // Path or URI to QSeal private key in PEM format
+  language: null,
+  paymentAuthRedirectUri: REDIRECT_URL,
+  paymentAuthState: "test",
 }
 
-/**
- * Bank connector specific settings
- */
-async function main() {
-  // Reading coutry code from console
-  var country = await readLine('Please enter ISO country code (e.g., SE):');
-  country = country.trim().toUpperCase();
-  // Initializing meta API
-  const metaApi = new enablebanking.MetaApi();
-  // Getting list of available connectors for the chosen country
-  const getConnectorsResult = await metaApi.getConnectors({ country: country });
-  if (!getConnectorsResult.connectors.length) {
-    console.log('No connectors available');
-    return;
-  }
-  console.log('Available connectors:')
-  getConnectorsResult.connectors.forEach(function(connector, index) {
-    console.log(index, connector.name);
+const readRedirectUrl = async (url, redirectUrl) => {
+  console.log(`Please, open this page in browser: ${url}`)
+  console.log("Log in, authenticate and copy/paste back the URL where you got redirected.")
+  return input(`URL (starts with ${redirectUrl}): `)
+}
+
+const input = async (query) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-  const connectorIndex = await readLine('Please enter connector index to choose:')
-  const connector = getConnectorsResult.connectors[parseInt(connectorIndex)];
-  if (connector === undefined) {
-    console.log('Wrong connector index');
-    return;
-  }
-  // Saving authentication meta (will be used later)
-  const authMeta = connector.authInfo[0].info;
-  const connectorSettings = settings[connector.name];
-  if (connectorSettings === undefined) {
-    console.log('No settings for the chosen connector');
-    return;
-  }
-  // Overriding country in the settings (but only if it's present)
-  if (connectorSettings.country !== undefined) {
-    connectorSettings.country = country;
-  }
-  // Initializing ApiClient for the chosen bank
-  const apiClient = new enablebanking.ApiClient(
-    connector.name,
-    // passing only values (so the order of the props matters and that's why constructFromObject is used)
-    Object.values(connectorSettings));
-  // Initializing authentication interface
-  const authApi = new enablebanking.AuthApi(apiClient);
-  // Setting user client information
-  await authApi.setClientInfo({
-    clientInfo: {
-      psuIpAddress: '10.10.10.10',
-      psuUserAgent: 'Mozilla'
-      // more parameters might be needed for some banks, so it's recommended to
-      // fill in all.
-    }
-  })
-  // Reading user ID in case it's required for user authentication
-  if (authMeta.userIdRequired) {
-    var userId = await readLine(
-      'Please enter user id (for sandboxes consult with the bank developer documentation):')
-  }
-  // Setting access validity for 2 days
-  const accessValidUntil = new Date();
-  accessValidUntil.setDate(accessValidUntil.getDate() + 2);
-  // Declaring base access scope
-  const access = new enablebanking.Access({
-    recurringIndicator: false,
-    validUntil: accessValidUntil
-  })
-  const getAuthResult = await authApi.getAuth({
-    state: 'test',
-    userId: userId,
-    access: access // will be ignored by the connectors where not used
-  });
-  var authorizationCode = '';
-  if (getAuthResult.url) {
-    console.log(
-      'Follow the URL to make user authorization '
-        + '(usually you need to do a few clicks in the UI):\n'
-        + getAuthResult.url
-    )
-    const redirectUrl = await readLine('Please enter the URL you were redirected to:');
-    // Parsing redirect url using special Auth API method (connector specific)
-    const parseRedirectUrlResult = await authApi.parseRedirectUrl(redirectUrl);
-    authorizationCode = parseRedirectUrlResult.code;
-  } else {
-    console.log(
-      'Decoupled authentication flow is used. '
-        + 'In sandbox environment authentication is likely to be omitted. '
-        + 'Please consult with the documentation for the chosen connector.'
-    )
-  }
-  // Requesting user access token
-  const makeTokenResponse = await authApi.makeToken(
-    'authorization_code',
-    authorizationCode,
-    { authEnv: getAuthResult.env }
-  );
-  console.log('User access token received:', makeTokenResponse.access_token);
-  // Initializing AISP interface. Normally in order to use AISP interface ApiClient needs
-  // to be initialized with accessToken, but here we are re-using existing ApiClient instance as
-  // the token is preserved inside it.
-  const aispApi = new enablebanking.AISPApi(apiClient);
-  var needAccountAccess = false;
-  // Need to call modifyConsents before getting account information
-  if (!authMeta.access) {
-    const modifyConsentsResult = await aispApi.modifyConsents({
-      access: access
-    });
-    console.log('User consent id:', modifyConsentsResult.consentId);
-    if (modifyConsentsResult._links.redirect) {
-      console.log(
-        'Follow the URL to confirm the consent: '
-          + modifyConsentsResult._links.redirect.href);
-      await readLine('Press ENTER after the consent is given');
-    } else {
-      // FIXME: this needs to be formalized
-      needAccountAccess = true;
+
+  return new Promise(resolve => rl.question(query, ans => {
+    rl.close();
+    resolve(ans);
+  }))
+}
+
+function sleep(seconds) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
+
+const getConnectorMeta = async (connectorName, connectorCountry) => {
+  const metaApi = new enablebanking.MetaApi(enablebanking.ApiClient())
+  for (const conn of (await metaApi.getConnectors({ country: connectorCountry })).connectors) {
+    if (conn.name == connectorName) {
+      return conn
     }
   }
-  // Getting list of user's bank accounts
-  const getAccountsResult = await aispApi.getAccounts();
-  console.log(getAccountsResult.accounts.length + ' account(s) available:');
-  getAccountsResult.accounts.forEach(function(account, index) {
-    console.log(index, account.accountId);
-  });
-  const accountIndex = await readLine('Please enter account index to choose:');
-  const account = getAccountsResult.accounts[parseInt(accountIndex.trim())];
-  if (account === undefined) {
-    console.log('Wrong account index');
-    return;
-  }
-  if (needAccountAccess) {
-    // FIXME: duplicated code to be unified
-    const modifyConsentsResultForAccount = await aispApi.modifyConsents({
-      access: new enablebanking.Access({
-        accounts: [account.accountId],
-        transactions: new enablebanking.TransactionsAccess(),
-        validUntil: accessValidUntil
-      })
-    });
-    console.log('User consent id:', modifyConsentsResultForAccount.consentId);
-    if (modifyConsentsResultForAccount._links.redirect) {
-      console.log(
-        'Follow the URL to confirm the consent: '
-          + modifyConsentsResultForAccount._links.redirect.href);
-      await readLine('Press ENTER after the consent is given');
-    }
-  }
-  // Getting list of account transactions
-  const getAccountTransactionsResult = await aispApi.getAccountTransactions(account.resourceId);
-  console.log(getAccountTransactionsResult.transactions.length + ' transaction(s) received:');
-  getAccountTransactionsResult.transactions.forEach(function(transaction, index) {
-    console.log(index, transaction);
-  });
+  throw new Error("Meta not found")
 };
 
-main().then(function() {
-  process.exit(0);
-}).catch(function(e) {
-  console.error(e);
-  process.exit(1);
+async function main() {
+  const apiMeta = await getConnectorMeta(CONNECTOR_NAME, CONNECTOR_COUNTRY);
+  const apiClient = new enablebanking.ApiClient(CONNECTOR_NAME, CONNECTOR_SETTINGS);
+  const authApi = new enablebanking.AuthApi(apiClient);
+  const clientInfo = new enablebanking.ClientInfo();
+  const connectorPsuHeaders = apiMeta.requiredPsuHeaders;
+  if (connectorPsuHeaders.includes('psuIpAddress')) {
+    clientInfo.psuIpAddress = '10.10.10.10'
+  }
+  if (connectorPsuHeaders.includes('psuUserAgent')) {
+    clientInfo.psuUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15'
+  }
+  await authApi.setClientInfo({ clientInfo: clientInfo })
+
+  const getAuthParams = { state: "test" }
+  if (apiMeta.authInfo[0].info.access) {
+    getAuthParams.access = new enablebanking.Access()
+  }
+  if (apiMeta.authInfo[0].info.userIdRequired) {
+    getAuthParams.userId = "someId"
+  }
+  if (apiMeta.authInfo[0].info.passwordRequired) {
+    getAuthParams.password = "somePassword"
+  }
+  const authResponse = await authApi.getAuth(getAuthParams);
+
+  let makeTokenResponse
+  if (authResponse.url) {
+    // if url is returned, then we are doing redirect flow
+    const redirectedUrl = await readRedirectUrl(authResponse.url, REDIRECT_URL)
+    const queryParams = await authApi.parseRedirectUrl(redirectedUrl)
+    console.log(`Parsed query: ${queryParams}`)
+    makeTokenResponse = await authApi.makeToken(
+      "authorization_code",
+      queryParams.code,
+      { authEnv: authResponse.env }
+    )
+  } else {
+    // decoupled flow
+    // Doing a retry to check periodically whether user has already authorized a consent
+    const sleepTime = 3
+    for (let i = 0; i < 3; i++) {
+      try {
+        makeTokenResponse = await authApi.makeToken(
+          "authorization_code",
+          "",
+          { authEnv: authResponse.env }
+        )
+        break
+      } catch (err) {
+        console.log(`Failed to get authorization code, sleeping for ${sleepTime} seconds`)
+        console.log(err)
+        await sleep(sleepTime)
+      }
+    }
+  }
+
+  console.log(`Token is: ${makeTokenResponse}`)
+
+  // apiClient has already accessToken and refreshTOken applied after call to makeToken()
+  const aispApi = new enablebanking.AISPApi(apiClient)
+
+  const validUntil = new Date()
+  const days = 89
+  validUntil.setDate(validUntil.getDate() + days)
+  if (apiMeta.modifyConsentsInfo[0].info.beforeAccounts) {
+    const access = new enablebanking.Access({
+      validUntil: validUntil
+    });
+    const consent = await aispApi.modifyConsents({ access: access })
+    console.log(`Consent: ${consent}`);
+    try {
+      const consentUrl = consent._links.redirect.href;
+      const redirectUrl = await readRedirectUrl(consentUrl, REDIRECT_URL)
+      console.log(`Redirect url: ${redirectUrl}`);
+    } catch (error) { }
+  }
+
+  const accounts = await aispApi.getAccounts();
+  console.log(accounts);
+
+  if (apiMeta.modifyConsentsInfo[0].info.beforeAccounts) {
+    const accountIds = getAccountsResult.accounts.map((el) => new enablebanking.AccountIdentification({ iban: el.accountId.iban }));
+    const access = new enablebanking.Access({
+      accounts: accountIds,
+      validUntil: validUntil
+    });
+    const consent = await aispApi.modifyConsents({ access: access })
+    console.log(`Consent: ${consent}`);
+    try {
+      const consentUrl = consent._links.redirect.href;
+      const redirectUrl = await readRedirectUrl(consentUrl, REDIRECT_URL)
+      console.log(`Redirect url: ${redirectUrl}`);
+    } catch (error) { }
+  }
+  for (const account of accounts.accounts) {
+    let transactions = await aispApi.getAccountTransactions(account.resourceId)
+    console.log(`Transactions info: ${transactions}`)
+    let balances = await aispApi.getAccountTransactions(account.resourceId)
+    console.log(`Balances info: ${balances}`)
+  }
+};
+
+main().then(function () {
+  console.log("All done!");
 });
